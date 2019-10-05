ee82c61bdddf9ddfb2994b8bdb22236157563e80
Erik Rekola <aimsocool@gmail.com>
2019-10-05 20:30:26 +0300

    Remove target from construct

--- src/Character.ts
+++ src/Character.ts
@@ -1,21 +1,25 @@
 import { GameObject } from "./types";
 import Position from "./Position";
+import { Radians } from "./constants";
 
 class Character implements GameObject {
   position: Position = null;
   isActive: boolean = false;
   size: number = 8 + Math.ceil(Math.random() * 10);
-  target: Position = null;
+  target: Character = null;
+  angle: number;
 
   constructor(x: number | Position = 0, y: number = 0) {
     if (typeof x === "number") {
-      const position = new Position(x, y);
-      this.position = position;
-      this.target = position;
+      this.position = new Position(x, y);
     } else {
       this.position = x;
-      this.target = x;
     }
+    this.angle = Radians.RIGHT;
+  }
+
+  get speed(): number {
+    return this.size / 10;
   }
 }
 
